10b2bc4b6c1d5d717422f7cf816336e791d35fac
onefiles <ginto0904@gmail.com>
2025-08-20 00:11:34 +0900

    Test deployment: Add fire emoji

--- lib/screens/history_screen_ios.dart
+++ lib/screens/history_screen_ios.dart
@@ -298,7 +298,7 @@ class _HistoryScreenIOSState extends State<HistoryScreenIOS> {
                   if (history.isEmpty) {
                     return const Center(
                       child: Text(
-                        'たこ焼き 🐙',
+                        'たこ焼き 🐙🔥',
                         style: TextStyle(color: CupertinoColors.systemGrey, fontSize: 16),
                       ),
                     );
